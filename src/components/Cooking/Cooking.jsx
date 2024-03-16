15a4d9a57acf84f244bf5f2de30562e3cd226336
Jannatul Ferdous Mirza <jfmirza1303@gmail.com>
2024-03-17 03:19:34 +0600

    added  minor corrections

--- src/components/Cooking/Cooking.jsx
+++ src/components/Cooking/Cooking.jsx
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 const Cooking = ({cooking, index}) => {
     const {name, time, calories}=cooking;
     return (
-        <tr className="bg-[#f8f7f2cc] text-center ">                        
+        <tr className="bg-[#f8f7f2cc] text-center">                        
             <th>{index+1}</th>
             <td>{name}</td>
             <td>{time} minutes</td>
-            <td>{calories} calories</td>
+            <td className='py-6'>{calories} calories</td>
         </tr>
     );
 };
